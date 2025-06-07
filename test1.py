2385ae6c2f2f529059315e07fb85db8b1477c60b
ashu-nair <ashithoshnair669@gmail.com>
2025-06-07 17:45:12 +0530

    Added Encryption and Versioning Logs

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 import csv
 from datetime import datetime
 
@@ -15,7 +16,8 @@ def scan_s3_buckets():
     for bucket in response['Buckets']:
         bucket_name = bucket['Name']
         status = "PRIVATE"
-
+        encrypt_status = "NO ENCRYPTION"
+        version_status = "UNKNOWN"
         # Check ACL
         try:
             acl = s3.get_bucket_acl(Bucket=bucket_name)
@@ -33,9 +35,39 @@ def scan_s3_buckets():
                 status = "PUBLIC via Policy"
         except:
             pass
-
         
-        results.append([bucket_name, status])
+        #Checking Encryption
+        try:
+            enc = s3.get_bucket_encryption(Bucket=bucket_name)
+            rules = enc['ServerSideEncryptionConfiguration']['Rules']
+            encrypt_status = "ENCRYPTION ENABLED"
+        except s3.exceptions.ClientError as e:
+            if "ServerSideEncryptionConfigurationNotFoundError" in str(e):
+                encrypt_status = "NO ENCRYPTION"
+            else:
+                encrypt_status = f"ERROR: {e.response['Error']['Code']}"
+        except Exception as e:
+            encrypt_status = f"ERROR: {str(e)}"
+
+        #Checking Versioning
+        try : 
+            vn = s3.get_bucket_versioning(Bucket = bucket_name)
+            if 'Status' in vn and vn['Status'] == 'Enabled':
+                version_status = 'Version Enabled'
+            elif 'Status' in vn and vn['Status'] == 'Suspended':
+                version_status = 'Version Suspended'
+            else:
+                version_status = 'Version Not Configured'
+        except ClientError as e:
+            error_code = e.response.get("Error", {}).get("Code")
+            if error_code == 'AccessDenied':
+                version_status = "ERROR (Versioning: Access Denied)"
+            else:
+                version_status = f"ERROR (Versioning: {error_code})"
+        except Exception as e:
+            version_status = f"ERROR (Versioning: {type(e).__name__})"
+        
+        results.append([bucket_name, status , encrypt_status,version_status])
 
     # For Timing
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
@@ -45,7 +77,7 @@ def scan_s3_buckets():
     # Save to CSV
     with open(filename, mode="w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["Bucket Name", "Status"])
+        writer.writerow(["Bucket Name", "Status", "Encryption" , "Versioning"])
         writer.writerows(results)
 
     print("\n✅ Scan complete. Results saved to 's3_results.csv'.")
